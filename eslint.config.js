f335c4b06d706efc8630c3793e432208655d94f4
BrunoCaputo <brunopc.dev@gmail.com>
2024-11-06 16:03:04 -0300

    chore: TS, Linter and Dependencies config

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,93 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import prettierRecommended from "eslint-plugin-prettier/recommended";
+import standard from "eslint-config-standard";
+import parser from "@typescript-eslint/parser";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import eslintReact from "eslint-plugin-react";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      prettierRecommended,
+      ...tseslint.configs.recommended,
+    ],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+        ...standard.globals,
+      },
+      parser,
+      parserOptions: {
+        ...standard.parserOptions,
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      "simple-import-sort": simpleImportSort,
+      standard: standard.plugins,
+      "jsx-a11y": jsxA11y,
+      react: eslintReact,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
+      "simple-import-sort/imports": "error",
+      "prettier/prettier": [
+        "error",
+        {
+          printWidth: 80,
+          tabWidth: 2,
+          singleQuote: true,
+          trailingComma: "all",
+          arrowParens: "always",
+          semi: false,
+          endOfLine: "auto",
+        },
+      ],
+      "react/self-closing-comp": "error",
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "jsx-a11y/alt-text": [
+        "warn",
+        {
+          elements: ["img"],
+          img: ["Image"],
+        },
+      ],
+      "jsx-a11y/aria-props": "warn",
+      "jsx-a11y/aria-proptypes": "warn",
+      "jsx-a11y/aria-unsupported-elements": "warn",
+      "jsx-a11y/role-has-required-aria-props": "warn",
+      "jsx-a11y/role-supports-aria-props": "warn",
+      "react/no-unknown-property": "error",
     },
+    settings: {
+      react: {
+        version: "detect",
+      },
+      "import/parsers": {
+        [import("@typescript-eslint/parser")]: [".ts", ".tsx", ".d.ts"],
+      },
+    },
+    ignores: ["node_modules"],
   },
-)
+);
