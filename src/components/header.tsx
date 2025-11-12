e7eda7d79e0580be172b15bd25fa3f37a59b9486
BrunoCaputo <brunopc.dev@gmail.com>
2025-11-12 00:56:51 -0300

    feat: Seller menu and logout

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,10 @@
-import { ChartArea, Package, Plus, User } from 'lucide-react'
+import { useMutation } from '@tanstack/react-query'
+import { ChartArea, LogOut, Package, Plus, User } from 'lucide-react'
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 import { Seller } from '@/@types/seller'
+import { signOut } from '@/api/auth/sign-out'
 import LogoIcon from '@/assets/LogoIcon.svg'
 import { NavLink } from '@/components/nav-link'
 
@@ -14,8 +17,19 @@ interface HeaderProps {
 export function Header({ seller }: HeaderProps) {
   const navigate = useNavigate()
 
+  const [showSellerMenu, setShowSellerMenu] = useState<boolean>(false)
+
+  const { mutateAsync: signOutFn } = useMutation({
+    mutationFn: signOut,
+  })
+
   function handleNewProduct() {
     navigate('/products/new')
+  }
+
+  async function handleLogout() {
+    await signOutFn()
+    navigate('/sign-in')
   }
 
   return (
@@ -45,15 +59,52 @@ export function Header({ seller }: HeaderProps) {
           Novo produto
         </Button>
 
-        <div className="flex h-12 w-12 cursor-pointer items-center justify-center rounded-xl bg-shape">
-          {seller?.avatar?.id ? (
-            <img
-              src={seller?.avatar?.url}
-              alt=""
-              className="h-full w-full rounded-xl object-cover"
-            />
-          ) : (
-            <User className="h-5 w-5 text-gray-200" />
+        <div className="relative">
+          <div
+            className="relative flex h-12 w-12 cursor-pointer items-center justify-center rounded-xl bg-shape"
+            onClick={() => setShowSellerMenu((prev) => !prev)}
+          >
+            {seller?.avatar?.id ? (
+              <img
+                src={seller?.avatar?.url}
+                alt=""
+                className="h-full w-full rounded-xl object-cover"
+              />
+            ) : (
+              <User className="h-5 w-5 text-gray-200" />
+            )}
+          </div>
+
+          {showSellerMenu && (
+            <div className="absolute right-0 top-0 flex w-[168px] translate-y-2/4 flex-col gap-5 rounded-xl bg-white p-4">
+              <div className="flex h-[34px] w-full items-center justify-start gap-3">
+                <div className="flex h-8 w-8 items-center justify-center rounded-xl bg-shape">
+                  {seller?.avatar?.id ? (
+                    <img
+                      src={seller?.avatar?.url}
+                      alt=""
+                      className="h-full w-full rounded-xl object-cover"
+                    />
+                  ) : (
+                    <User className="h-5 w-5 text-gray-200" />
+                  )}
+                </div>
+                <p className="line-clamp-2 text-body-sm text-gray-300">
+                  {seller?.name}
+                </p>
+              </div>
+
+              <div className="h-[1px] w-full bg-shape" />
+
+              {/* LOGOUT */}
+              <div
+                className="flex cursor-pointer items-center justify-between p-[2px] text-orange-base"
+                onClick={handleLogout}
+              >
+                <p className="text-action-sm">Sair</p>
+                <LogOut className="h-5 w-5" />
+              </div>
+            </div>
           )}
         </div>
       </section>
